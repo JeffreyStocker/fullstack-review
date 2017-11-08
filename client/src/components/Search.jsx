d9e33e7a452f99c4582cc4c44b7100b24056f869
Jeff <jeffreyRstocker@gmail.com>
2017-11-08 15:48:41 -0800

    Added enter when in the search input

--- client/src/components/Search.jsx
+++ client/src/components/Search.jsx
@@ -8,12 +8,19 @@ class Search extends React.Component {
     }
     this.search = this.search.bind(this)
     this.onChange = this.onChange.bind(this)
+    // this.onKeyPress = this.onChange.bind(this)
   }
 
+
   onChange (e) {
-    this.setState({
-      term: e.target.value
-    });
+    if(e.charCode==13){
+      this.props.onSearch(this.state.term);
+    } else {
+      this.setState({
+        term: e.target.value
+      });
+      // console.log (this.state.term)
+    }
   }
 
   search() {
@@ -23,10 +30,12 @@ class Search extends React.Component {
   render() {
     return (<div>
       <h4>Add more repos!</h4>
-      Enter a github username: <input value={this.state.terms} onChange={this.onChange}/>       
+      Enter a github username: <input value={this.state.terms} onChange={this.onChange} onKeyPress={this.onChange}/>       
       <button onClick={this.search}> Add Repos </button>
     </div>) 
   }
 }
 
 export default Search;
+
+
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -10,16 +10,15 @@ class App extends React.Component {
     this.state = { 
       repos: []
     }
-
-  }
+  var search = this.search.bind(this);
+ }
 
   search (term) {
     console.log(`${term} was searched`);
-    // TODO
-  }
-
-  onSearch () {
-    console.log ('onSearch')
+    var url = 'http://localhost:1128/repos';
+      $.post(url, term, function (data, textStatus, jqXHR) {
+        console.log ('sendRequestToServer Success data:', data, '||', textStatus)
+      })
   }
 
   render () {
@@ -32,3 +31,57 @@ class App extends React.Component {
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
+
+
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import $ from 'jquery';
+// import Search from './components/Search.jsx';
+// import RepoList from './components/RepoList.jsx';
+
+// class App extends React.Component {
+//   constructor(props) {
+//     super(props);
+//     this.state = { 
+//       repos: []
+//     }
+    
+//     var sendRequestToServer = function (searchData) {
+//       url = 'localhost:1128';
+//       // $.ajax({
+//       //   url: 'localhost:1128',
+//       //   method: 'get',
+//       //   // data: somehting // will be converted to a query string
+//       //   error: function (jqXHR, textStatus, error) {
+//       //     console.log (jqXHR, textStatus, error)
+//       //   },
+//       //   success: function (data, textStatus, jqXHR) {
+//       //     console.log ('data', data)
+//       //     console.log ('textStatus', textStatus)
+//       //   }
+//       // })
+//       $.post(url, searchData, function (data, textStatus, jqXHR) {
+//         console.log ('sendRequestToServer Success data:', data, '||', textStatus)
+//       })
+  
+//     }
+//   }
+
+
+//   onSearch (term) {
+//     console.log(`${term} was searched`);
+//     $.post('http://localhost:1128/repos', term, function (data, textStatus, jqXHR) {
+//       console.log ('sendRequestToServer Success data:', data, '||', textStatus)
+//     })
+//   }
+
+//   render () {
+//     return (<div>
+//       <h1>Github Fetcher</h1>
+//       <RepoList repos={this.state.repos}/>
+//       <Search onSearch={this.onSearch}/>
+//     </div>)
+//   }
+// }
+
+// ReactDOM.render(<App />, document.getElementById('app'));
