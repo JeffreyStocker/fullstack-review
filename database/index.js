33f51cfb552690345ddd4aeaf3f50d68642974f3
Jeff <jeffreyRstocker@gmail.com>
2017-11-08 17:09:36 -0800

    Updated to correct to use : instead of =

--- database/index.js
+++ database/index.js
@@ -12,21 +12,21 @@ let repoSchema = mongoose.Schema({
     // Mixed
     // ObjectId
     // Array
-  id = number, 
-  full_name = string,
-  name = string,
-  html_url = string, 
-  description = string, 
-  size = number,
-  watchers_count = number,
-  stargazers_count = number,
-  jsonData = string,
-  created_at = date,
-  updated_at = date,
-  pushed_at = date,
+  id : {type: Number, unique: true}, //note double check this 
+  full_name : String,
+  name : String,
+  html_url : String, 
+  description : String, 
+  size : Number,
+  watchers_count : Number,
+  stargazers_count : Number,
+  jsonData : String,
+  created_at : Date,
+  updated_at : Date,
+  pushed_at : Date,
 
-  ownerId = number,
-  owner_login = string,
+  ownerId : Number,
+  owner_login : String,
 });
 
 let Repo = mongoose.model('Repo', repoSchema);  //compiles the schema into a model;
@@ -41,21 +41,21 @@ let save = (repoInfo) => {
   
   //save an indivdual repo information to the mongoDB
   let saveObject = {
-    id = repoInfo.id, 
-    full_name = repoInfo.full_name,
-    name = repoInfo.name,
-    html_url = repoInfo.html_url,
-    description = repoInfo.description,
-    size = repoInfo.size,
-    watchers_count = repoInfo.watchers_count,
-    stargazers_count = repoInfo.stargazers_count,
-    created_at = repoInfo.created_at,
-    updated_at = repoInfo.updated_at,
-    pushed_at = repoInfo.pushed_at,
-    jsonData = JSON.stringify(repoInfo),
+    id : repoInfo.id, 
+    full_name : repoInfo.full_name,
+    name : repoInfo.name,
+    html_url : repoInfo.html_url,
+    description : repoInfo.description,
+    size : repoInfo.size,
+    watchers_count : repoInfo.watchers_count,
+    stargazers_count : repoInfo.stargazers_count,
+    created_at : repoInfo.created_at,
+    updated_at : repoInfo.updated_at,
+    pushed_at : repoInfo.pushed_at,
+    jsonData : JSON.stringify(repoInfo),
 
-    ownerId = repoInfo.owner.id,
-    owner_login = repoInfo.login,
+    ownerId : repoInfo.owner.id,
+    owner_login : repoInfo.login,
   }; 
   //one way to save
   Repo.create (saveObject, (err, response) => { 
