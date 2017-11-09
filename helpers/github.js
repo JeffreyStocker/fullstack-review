213ec6d1ef82d8328ea8272c214177527a267e28
Jeff <jeffreyRstocker@gmail.com>
2017-11-08 19:11:10 -0800

    Fixed get call to github

--- helpers/github.js
+++ helpers/github.js
@@ -1,44 +1,48 @@
 const request = require('request');
 const config = require('../config.js');
-const https = require('https')
+const bluebird = require('bluebird')
+
 // const app = require('../server/index.js')
 
 
-
-//////////////need to figure this out
-let getReposByUsername = (userName) => {
-  // TODO - Use the request module to request repos for a specific
-  // user from the github API
-
-  // The options object has been provided to help you out, 
-  // but you'll have to fill in the URL
-  var userName = 'jeffreystocker'
-  var callback = (error, data) => {
-    console.log (error)
-  }
-  let options = {
-    // url: `https://api.github.com/${userName}/repos`,
-    //users/jeffreystocker/repos
-    // host: 'https://api.github.com',
-    host: 'api.github.com',
-    path: '/users/' + userName + '/repos/',
-    // method: 'GET', 
-    headers: {
-      'User-Agent': 'request',
-      'Authorization': `token ${config.TOKEN}`
-    },
-    // callback: callback
-  };
-
-var talkToGit = https.request(options, (error, data) => {
-  if (error) {
-    console.error ('https: get error :', error)
-  } else {
-    console.error ('https: get data :', data)
-  }
-})
-
+var githubMiddle = function (req, res, next) {
+  
 }
-// getReposByUsername()
 
-module.exports.getReposByUsername = getReposByUsername;
+
+let getReposByUsername = (userName) => {
+  return new Promise ((resolve, revoke) => {
+
+    // TODO - Use the request module to request repos for a specific
+    // user from the github API
+    
+    // The options object has been provided to help you out, 
+    // but you'll have to fill in the URL
+    var userName = 'jeffreystocker'
+    // var callback = (error, data) => {
+    //   console.log (error)
+    // }
+    let options = {
+      url: `https://api.github.com/users/${userName}/repos`,
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': `token ${config.TOKEN}`
+      },
+    };
+    
+    var talkToGit = request(options, (error, data) => {
+      if (error) {
+        revoke (error)
+        // console.error ('https: get error :', error)
+      } else {
+        resolve(JSON.parse(data))
+        // console.error ('https: get data :', data)
+      }
+    })
+  })
+}
+  
+  /////testing
+  // getReposByUsername()
+  
+  module.exports.getReposByUsername = getReposByUsername;
