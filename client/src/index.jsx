1d113d15f01b560a3fc5ec8248e902329353752f
Jeff <jeffreyRstocker@gmail.com>
2017-11-09 13:44:43 -0800

    Added a get for client to get repos when started

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -8,80 +8,47 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      repos: []
+      repos: [],
+      sort: 'first'
     }
   var search = this.search.bind(this);
- }
+  var currentThis = this;
+  // this.trySetstate = this.trySetstate.bind(this)
+  // console.log ('testThis ', this)
+  // this.trySetstate()
+  this.getReposFromServer()
+}
+
+  getReposFromServer () {
+    var storedthis = this;
+    $.get('http://localhost:1128/repos','', function (data, textStatus) {
+      // console.log ('received data', data)
+      storedthis.setState({ repos: data })
+    } )
+  }
 
   search (term) {
-    console.log(`${term} was searched`);
+    var tempThis = this
+    // console.log(`${term} was searched`);
     var url = 'http://localhost:1128/repos';
-      $.post(url, term, function (data, textStatus, jqXHR) {
-        console.log ('sendRequestToServer Success data:', data, '||', textStatus).type('form')
+      $.post(url, term, function (data, textStatus) {
+        // console.log ('sendRequestToServer Success data:', data, '||', textStatus)
+        if (textStatus === 'success') {
+          // console.log (this.state)
+          tempThis.setState({
+            repos : data
+          })
+        }
       })
   }
 
   render () {
     return (<div>
       <h1>Github Fetcher</h1>
+      <Search onSearch={this.search.bind(this)}/>
       <RepoList repos={this.state.repos}/>
-      <Search onSearch={this.search}/>
     </div>)
   }
 }
 
 ReactDOM.render(<App />, document.getElementById('app'));
-
-
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import $ from 'jquery';
-// import Search from './components/Search.jsx';
-// import RepoList from './components/RepoList.jsx';
-
-// class App extends React.Component {
-//   constructor(props) {
-//     super(props);
-//     this.state = { 
-//       repos: []
-//     }
-    
-//     var sendRequestToServer = function (searchData) {
-//       url = 'localhost:1128';
-//       // $.ajax({
-//       //   url: 'localhost:1128',
-//       //   method: 'get',
-//       //   // data: somehting // will be converted to a query string
-//       //   error: function (jqXHR, textStatus, error) {
-//       //     console.log (jqXHR, textStatus, error)
-//       //   },
-//       //   success: function (data, textStatus, jqXHR) {
-//       //     console.log ('data', data)
-//       //     console.log ('textStatus', textStatus)
-//       //   }
-//       // })
-//       $.post(url, searchData, function (data, textStatus, jqXHR) {
-//         console.log ('sendRequestToServer Success data:', data, '||', textStatus)
-//       })
-  
-//     }
-//   }
-
-
-//   onSearch (term) {
-//     console.log(`${term} was searched`);
-//     $.post('http://localhost:1128/repos', term, function (data, textStatus, jqXHR) {
-//       console.log ('sendRequestToServer Success data:', data, '||', textStatus)
-//     })
-//   }
-
-//   render () {
-//     return (<div>
-//       <h1>Github Fetcher</h1>
-//       <RepoList repos={this.state.repos}/>
-//       <Search onSearch={this.onSearch}/>
-//     </div>)
-//   }
-// }
-
-// ReactDOM.render(<App />, document.getElementById('app'));
