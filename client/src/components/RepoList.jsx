4f8fc53116007da1a0dbcefb90a7cf794c187386
Jeff <jeffreyRstocker@gmail.com>
2017-11-09 12:20:54 -0800

    Added dynamic adding of Repos to the view

--- client/src/components/RepoList.jsx
+++ client/src/components/RepoList.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import RepoView from './repoView.jsx';
 
 const RepoList = (props) => (
   <div>
-    <h4> Repo List Component </h4>
-    There are {props.repos.length} repos.
+    <div>
+      <h4> Repo List Component </h4>
+      There are {props.repos.length} repos.
+    </div>
+    {
+      props.repos.map((repo, index) => {
+        if (index < 25) {
+          return <RepoView repo={repo} key={repo.id} />
+        }
+      })
+    }
   </div>
 )
 
